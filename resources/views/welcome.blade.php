679052c0eee7209097125a1cfb4aa5ff320935e5
Roman Sliusar <r.sliusar@astoundcommerce.com>
2023-06-12 15:25:28 +0300

    Removed service container example

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,11 +17,6 @@
     </head>
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-            <ul class="max-w-7xl mx-auto p-6 lg:p-8">
-                @foreach($userList as $userName => $userPassword)
-                    <li> {{ $userName }} has {{ $userPassword }}</li>
-                @endforeach
-            </ul>
         @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
                     @auth
