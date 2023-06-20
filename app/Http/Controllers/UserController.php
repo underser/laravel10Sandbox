41bf296dfdf165ec7b2c0892e4230084f0e144ee
Roman Sliusar <r.sliusar@astoundcommerce.com>
2023-06-20 20:20:56 +0300

    task 9

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::query()->whereHas('projects', function (Builder $query) {
+            $query->where('project_id', '!=', null);
+        })->get();
 
         return view('users.index', compact('users'));
     }
