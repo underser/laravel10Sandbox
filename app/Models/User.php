ac146c1e1d57a819727c955139da17cf599b207a
Roman Sliusar <r.sliusar@astoundcommerce.com>
2023-06-20 19:25:26 +0300

    task 4

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,4 +57,9 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Project::class)->withPivot('start_date');
     }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'users_roles');
+    }
 }
