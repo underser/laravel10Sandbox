97910881297a4927fdc3026683f666555c5ae111
Roman Sliusar <r.sliusar@astoundcommerce.com>
2023-06-24 16:48:36 +0300

    repo basic set-up

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
+
+Route::post('projects', [\App\Http\Controllers\ProjectController::class, 'store']);
+
+Route::get('houses/download/{house}', [\App\Http\Controllers\HouseController::class, 'download']);
+Route::resource('houses', \App\Http\Controllers\HouseController::class);
+
+Route::resource('offices', \App\Http\Controllers\OfficeController::class);
+
+Route::post('shops', [\App\Http\Controllers\ShopController::class, 'store']);
+
+Route::resource('companies', \App\Http\Controllers\CompanyController::class);
