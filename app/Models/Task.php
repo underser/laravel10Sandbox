1c853ddb8985d63b0c921ce8728ae356f4e8091b
Roman Sliusar <r.sliusar@astoundcommerce.com>
2023-07-17 17:39:23 +0300

    Created DB tables, corresponding models and factories;

---
+++ app/Models/Task.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function status(): BelongsTo
+    {
+        return $this->belongsTo(TaskStatus::class);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Client;
+use App\Models\Project;
+use App\Models\ProjectStatus;
+use App\Models\Task;
+use App\Models\TaskStatus;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +18,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $commonStatuses = collect(['Open', 'In Progress', 'Postponed', 'Estimation', 'Done', 'Closed']);
+        $userIds = User::factory(30)->create()->modelKeys();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $commonStatuses->each(function ($status) {
+            ProjectStatus::factory()->create([
+                'status' => $status
+            ]);
+            TaskStatus::factory()->create([
+                'status' => $status
+            ]);
+        });
+
+        $commonStatuses->merge(['QA verification', 'Question', 'Implemented'])->each(function ($status) {
+            TaskStatus::factory()->create([
+                'status' => $status
+            ]);
+        });
+
+        $projectStatuses = ProjectStatus::all();
+        $taskStatuses = TaskStatus::all();
+
+        foreach (Client::factory(200)->create() as $client) {
+            $project = Project::factory()->create([
+                'user_id' => $userIds[array_rand($userIds)],
+                'client_id' => $client->id,
+                'project_status_id' => $projectStatuses->random(1)->first()->id
+            ]);
+
+            Task::factory(random_int(10, 40))->create([
+                'user_id' => $userIds[array_rand($userIds)],
+                'project_id' => $project->id,
+                'task_status_id' => $taskStatuses->random(1)->first()->id,
+            ]);
+        }
     }
 }
