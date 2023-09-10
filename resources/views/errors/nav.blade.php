123adb39c59b24dc04d5c117dc4723f36b82b2ff
Roman Sliusar <r.sliusar@astoundcommerce.com>
2023-09-10 18:00:12 +0300

    Removed old dashboard

--- resources/views/errors/nav.blade.php
+++ resources/views/errors/nav.blade.php
@@ -1,5 +1,5 @@
 <ul class="pt-2 flex justify-center">
-    <li><a href="{{ route('dashboard') }}"  class="text-sm pr-2 hover:underline">{{ __('Dashboard') }}</a></li>
+    <li><a href="{{ route('filament.admin.pages.dashboard') }}"  class="text-sm pr-2 hover:underline">{{ __('Dashboard') }}</a></li>
     @if(url()->previous() !== request()->url())
         <li><a href="{{ url()->previous() }}" class="text-sm hover:underline">{{ __('Go Back') }}</a></li>
     @endif
--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,5 +1,5 @@
 <div class="flex py-2">
-    <img src="{{ url('/logo.svg') }}" alt="logo" class="w-12 h-12 pr-2">
+    <img src="{{ url('/logo.svg') }}" alt="logo" style="width: 12px; height: 12px;">
     <p class="flex-1 font-bold uppercase text-xs tracking-wider">
         {{ __('WATERMELON CRM') }}
     </p>
